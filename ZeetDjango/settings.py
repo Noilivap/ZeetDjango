14d48c21e64c1d84d6c7a4b4e568d3fe0669e79c
Apoorv Garg <u.garg.10@gmail.com>
2020-12-03 21:09:33 +0530

    Update settings.py

--- ZeetDjango/settings.py
+++ ZeetDjango/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o!owpdk_h1qpgl3a#ap=f_j&^49jn&iw#&e=e8ga-84ni%%uvz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
